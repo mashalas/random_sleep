c22e0ed90aabcc39030b674023f3e927fdad3b93
Mashalas Alexey <mashalas_aa@irkut.ru>
2021-10-25 14:25:46 +0800

    use measures: seconds, minutes, hours and days

--- random_sleep.pl
+++ random_sleep.pl
@@ -44,9 +44,36 @@ my $min_value = shift;
 my $max_value = shift;
 my $result;
 
+  return $min_value if ($min_value == $max_value); # оба числа границ совпадают
   $result = $min_value + ($max_value - $min_value)*rand();
   $result = round_int($result);
   return $result;
+}
+
+sub random_test()
+{
+my $N = 500;
+my $lower_limit = -50;
+my $upper_limit = -10;
+my ($min_x, $max_x);
+my $avg = 0.0;
+
+  for (my $i=0; $i<$N; $i++) {
+    my $x = get_random_int($lower_limit, $upper_limit);
+    print "$x ";
+    $avg += $x;
+    if ($i == 0) {
+      $min_x = $x;
+      $max_x = $x;
+    }
+    else {
+      $min_x = $x if ($x < $min_x);
+      $max_x = $x if ($x > $max_x);
+    }
+  }
+  $avg /= $N;
+  print "\n";
+  print "min_x=$min_x  max_=$max_x  avg=$avg\n";
 }
 
 #-------------------------------------¬ернуть длину строки--------------------------------------
@@ -76,11 +103,16 @@ my $result = $src_str;
 }
 
 #------------------------“екущие дата/врем€ в формате yyyy-mm-dd hh:mm:ss--------------------------
-sub get_current_timestamp()
+sub get_timestamp
 {
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+my $custom_time = shift;
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst);# = localtime();
 my $result;
 
+  if ($custom_time == undef) {
+    $custom_time = time();
+  }
+  ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($custom_time);
   $result = 1900 + $year;
   $result .= "-" . append_before_str($mon, "0", 2);
   $result .= "-" . append_before_str($mday, "0", 2);
@@ -111,7 +143,7 @@ sub swap
 }
 
 #---------—корректировать очерЄдность параметров (verbose может быть первым, вторым или последним)---------
-sub correct_arguments_order($$$)
+sub correct_verbosity_argument_placement($$$)
 {
   my $a = shift;
   my $b = shift;
@@ -120,13 +152,13 @@ sub correct_arguments_order($$$)
 
   if ($c ne "") {
     # передано 3 параметра
-    if ($a =~ /^-?v$/i || $a =~ /^-?verbose/i) {
+    if ($a =~ /^-?v$/i || $a =~ /^-{0,2}verbose$/i) {
       # в первом параметре флаг v/verbose
       $min_duration = $b;
       $max_duration = $c;
       $verbose = $a;
     }
-    elsif ($b =~ /^-?v$/i || $b =~ /^-?verbose/i) {
+    elsif ($b =~ /^-?v$/i || $b =~ /^-{0,2}verbose$/i) {
       # во втором параметре флаг v/verbose
       $min_duration = $a;
       $max_duration = $c;
@@ -144,7 +176,7 @@ sub correct_arguments_order($$$)
     $verbose = "";
   }
   if ($verbose ne "") {
-    $verbose =~ s/^-//;
+    $verbose =~ s/^-+//;
     $verbose = lc($verbose);
   }
   return ($min_duration, $max_duration, $verbose);
@@ -152,9 +184,9 @@ sub correct_arguments_order($$$)
 
 sub correct_duration_order($$)
 {
-  my $a = shift;
-  my $b = shift;
-  my ($min_duration, $max_duration);
+my $a = shift;
+my $b = shift;
+my ($min_duration, $max_duration);
 
   if ($a <= $b) {
     $min_duration = $a;
@@ -167,72 +199,120 @@ sub correct_duration_order($$)
   return ($min_duration, $max_duration);
 }
 
+sub get_duration_seconds($)
+{
+my $raw_duration = shift;
+my $seconds_duration = -1; # если неверный формат (переданный параметр не €вл€етс€ числом с одним из суффиксов smhd соответствующих second/minute/hour/day, то функци€ возвратит -1
+my ($value, $measure);
+my $measure_size = 1;
+
+  if ($raw_duration =~ /^(\d+)([smhd]?)$/i) {
+    # указана единица измерени€
+    $value = $1;
+    $value *= 1;
+    $measure = $2;
+    $measure = lc($measure);
+    #print "empty measure\n" if ($measure eq "");
+    #print "[$value] [$measure]\n";
+    $measure_size = 60    if ($measure eq "m"); # минуты
+    $measure_size = 3600  if ($measure eq "h"); # часы
+    $measure_size = 86400 if ($measure eq "d"); # дни
+    $seconds_duration = $value * $measure_size;
+  }
+  return ($seconds_duration, $measure_size);
+}
+
+sub get_measure_name
+{
+my $measure_size = shift;
+  
+  return "minute" if ($measure_size == 60);
+  return "hour" if ($measure_size == 3600);
+  return "day" if ($measure_size == 86400);
+  return "second";
+}
+
 #-----------------ѕауза длительностью случайное количество секунд (в заданном интервале)------------
 sub random_sleep
 {
+my $ERROR_wrong_format_for_min_duration = -1;
+my $ERROR_wrong_format_for_max_duration = -2;
+my $NO_ERROR = 0;
 my $min_duration = shift;
 my $max_duration = shift;
 my $verbose = shift;
 my $duration;
 my $s;
+my ($min_duration_measure_size, $max_duration_measure_size, $measure_size);
+my $atom_sleep;
+my ($min_intervals_count, $max_intervals_count, $intervals_count);
 
-  #print "!!!1 $min_duration, $max_duration, $verbose\n";
-  ($min_duration, $max_duration, $verbose) = correct_arguments_order($min_duration, $max_duration, $verbose);
-  #print "!!!2 $min_duration, $max_duration, $verbose\n";
-  ($min_duration, $max_duration) = correct_duration_order($min_duration, $max_duration);
-  #print "!!!3 $min_duration, $max_duration, $verbose\n";
-
-  if (is_int_number($min_duration) == 0) {
-    print "ERROR: \"min_duration\" is not integer.\n";
-    return 2;
-  }
-  if (is_int_number($max_duration) == 0) {
-    print "ERROR: \"max_duration\" is not integer.\n";
-    return 3;
-  }
-
-  $min_duration *= 1;
-  $max_duration *= 1;
-
-  if ($min_duration < 0) {
-    print "ERROR: \"min_duration\" can not be negative.\n";
-    return 4;
-  }
-  if ($max_duration < 0) {
-    print "ERROR: \"max_duration\" can not be negative.\n";
-    return 5;
-  }
+  #print "QQQ1 $min_duration, $max_duration, $verbose\n";
+  # [-]v|verbose если не последним параметром, то учесть это
+  ($min_duration, $max_duration, $verbose) = correct_verbosity_argument_placement($min_duration, $max_duration, $verbose);
+  #print "QQQ2 $min_duration, $max_duration, $verbose\n";
   
+  ($min_duration, $min_duration_measure_size) = get_duration_seconds($min_duration);
+  if ($min_duration < 0) {
+    print "ERROR: wrong format for min_duration.\n";
+    return $ERROR_wrong_format_for_min_duration;
+  }
+
+  ($max_duration, $max_duration_measure_size) = get_duration_seconds($max_duration);
+  if ($max_duration < 0) {
+    print "ERROR: wrong format for max_duration.\n";
+    return $ERROR_wrong_format_for_max_duration;
+  }
+
+  #($min_duration, $max_duration) = correct_duration_order($min_duration, $max_duration);
   if ($min_duration > $max_duration) {
-    print "ERROR: \"min_duration\" is greater than \"max_duration\"\n";
-    return 6;
+    # помен€ть пор€док очерЄдность минимальной и максимвльной продолжительности (сначала должна быть минимальна€)
+    swap($min_duration, $max_duration);
   }
-  elsif ($min_duration == $max_duration) {
-    $duration = $min_duration;
+  $measure_size = $min_duration_measure_size;
+  $measure_size = $max_duration_measure_size if ($max_duration_measure_size < $min_duration_measure_size);
+  $min_intervals_count = $min_duration / $measure_size;
+  $max_intervals_count = $max_duration / $measure_size;
+  $intervals_count = get_random_int($min_intervals_count, $max_intervals_count);
+  #print "QQQ3 min_duration=$min_duration, max_duration=$max_duration, verbose=$verbose measure_size=$measure_size min_intervals_count=$min_intervals_count max_intervals_count=$max_intervals_count intervals_count=$intervals_count\n";
+
+  if ($verbose eq "v" || $verbose eq "verbose") {
+    #если подробный режим - вывести сколько интервалов ожидани€ по 1 единице измерени€ и сколько это всего займЄт секунд
+    my $total_sleep_duration_seconds = $intervals_count * $measure_size; # обща€ продолжительность сна в секундах
+    my $wake_time = time() + $total_sleep_duration_seconds;
+    my $wake_timestamp = get_timestamp($wake_time);
+
+    if ($measure_size > 1) {
+      # интервалы дольше 1 секунды (минута, час, день)
+      my $measure_name = get_measure_name($measure_size);
+      $measure_name .= "s";
+      print "sleep $intervals_count times by 1 $measure_name  (totaly $total_sleep_duration_seconds seconds)\n";
+    }
+    else {
+      # интервалы - секунды
+      print "sleep seconds: $total_sleep_duration_seconds\n";
+    }
+    print "wake at $wake_timestamp\n";
   }
-  else {
-    $duration = get_random_int($min_duration, $max_duration);
+
+  for (my $i=$intervals_count; $i>=1; $i--) {
+    # цикл сна
+    if ($verbose eq "verbose") {
+      $s = "[" . get_timestamp() . "]";
+      $s .= "\t" . "$i / $intervals_count";
+      print "$s\n";
+    }
+    sleep($measure_size);
   }
 
   if ($verbose eq "verbose") {
-    print "sleep duration = $duration\n";
-    for (my $i=$duration; $i>=1; $i--) {
-      $s = "[" . get_current_timestamp() . "]";
-      $s .= "\t" . "$i / $duration";
-      print "$s\n";
-      sleep(1);
-    }
-    print "\n";
+    # ввести врем€, когда закончили сон, если включен очень расширенный режим
+    $s = "[" . get_timestamp() . "]";
+    $s .= "\t" . "complete";
+    print "$s\n";
   }
-  elsif ($verbose eq "v") {
-    #print "sleep duration = $duration\n" if ($verbose == "v");
-    print "sleep duration = $duration\n";
-    sleep($duration);
-  }
-  else {
-    sleep($duration);
-  }
-  return 0;
+
+  return $NO_ERROR;
 }
 
 #-----------------------------------------main--------------------------------------
@@ -245,7 +325,16 @@ my $s;
 #my $a = get_random_int($ARGV[0], $ARGV[1]); print "$a\n";
 #}
 #my $s = append_before_str("qwe", "_", 5); print "$s\n";
-#my $s = get_current_timestamp(); print "$s\n";
+#my $s = get_timestamp(); print "$s\n";
+#exit(0);
+
+#my $a = time();
+#my $b = $a + 5;
+#my $c = localtime();
+#my $d = localtime($b);
+#my $s1 = get_timestamp();
+#my $s2 = get_timestamp($b);
+#print "$a $b $c $d $s1 $s2\n";
 #exit(0);
 
 if ($#ARGV == -1) {
